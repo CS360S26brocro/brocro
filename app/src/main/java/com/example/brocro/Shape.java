848c180a76f05655556871a14ad09c42ea26cc4b
Mahad Raza Ali <m.mahadraza@gmail.com>
2026-02-15 00:01:01 +0500

    fix, add conflict

--- app/src/main/java/com/example/brocro/Shape.java
+++ app/src/main/java/com/example/brocro/Shape.java
@@ -2,9 +2,12 @@ package com.example.brocro;
 
 public class Shape {
 
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     int x;
     int y;
-
     String color = "green";
-
 }
